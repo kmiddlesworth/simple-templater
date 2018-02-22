f5adf52282a9ff4416b411ca78d0a2caa2932b51
Kevin Middlesworth <kmiddlesworth@gmail.com>
2018-02-21 23:38:44 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const del = require('del');
+const fs = require('fs');
+const shell = require('shelljs');
+const app = express();
+
+
+app.use(bodyParser.json({limit: '50mb'}));       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true,
+  limit: '50mb'
+})); 
+
+app.get('/zip', (req, res) => {
+	res.sendFile(process.env.PWD + '/files.zip');
+})
+
+app.get('/', (req, res) => res.sendFile(process.env.PWD + '/app.html'));
+app.post('/', (req, res) => {
+
+// authenticate
+if (process.env.PASSWORD && process.env.USERNAME) {
+	authObj = {};
+	authObj[process.env.USERNAME] = process.env.PASSWORD;
+	app.use(basicAuth({
+	    users: authObj,
+	    challenge: true
+	}));
+}
+
+
+	try {
+
+		del(['files/*', '*.zip']).then(paths => {
+
+			let fileTemplate = req.body.file;
+			let htmlTemplate = req.body.html;
+
+			JSON.parse(req.body.data).forEach((item) => {
+
+				let file = function(){return fileTemplate }();
+				let body = function(){return htmlTemplate }();
+
+				for (let key in item) {
+					file = keyReplace(key, item[key], file);
+					body = keyReplace(key, item[key], body);
+				}
+
+				fs.writeFileSync('files/' + file, body);
+
+			});
+
+			shell.exec('zip -r files.zip files', function(code, stdout, stderr) {
+				res.redirect('/zip');
+			});
+			
+		});
+
+		function keyReplace(key, value, template){
+			key = '{{' + key + '}}';
+			return template.split(key).join(value);	
+		}
+
+	} catch(e){
+		console.log('error');
+		res.sendFile(process.env.PWD + '/app.html');
+	}
+
+});
+
+app.listen(process.env.PORT || 8085, () => console.log('Example app listening on port 8085!'))
